cf3cdf03d8a07138386410f5ddfe41ae95ffc923
Ryan Bott <ryan@ryanbott.com>
2022-08-18 21:15:02 -0500

    remove purge from tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  purge: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
